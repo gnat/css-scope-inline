07f5dc113c2fd2d4b20db0e481f21af49ca52de2
Nathaniel Sabanski <sabanski.n@gmail.com>
2023-09-05 11:16:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ new MutationObserver((mutations, observer) => {
 			if (!addedNode.parentNode || addedNode.parentNode?.tagName === 'HEAD') continue // Must have parent. Don't style <head>
 			cssScopeCount++ // Ready.
 			addedNode.parentNode.classList.add('self_'+cssScopeCount) // Make unique scope, example: .self_1234
-			var pattern = new RegExp('(\.this|\.self|\.me)(?![A-Za-z0-9\_\-])', 'g') // Can use: .this .self .me
+			var pattern = new RegExp('(\\.this|\\.self|\\.me)(?![A-Za-z0-9\_\-])', 'g') // Can use: .this .self .me
 			addedNode.textContent = addedNode.textContent.replace(pattern, '.self_'+cssScopeCount)
 		}
 	}
